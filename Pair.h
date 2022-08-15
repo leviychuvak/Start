c6469c18dfee31fb32e1becd0a212342664545b8
leviychuvak <anton1234maistrenko@gmail.com>
2022-08-15 21:35:27 +0300

    add task #2

---
+++ Pair.h
@@ -0,0 +1,22 @@
+#ifndef TEST13_1_PAIR_H
+#define TEST13_1_PAIR_H
+
+template<class F, class S>
+class Pair {
+private:
+    F m_first;
+    S m_second;
+
+public:
+    Pair(const F &aFirst, const S &aSecond)
+        : m_first(aFirst), m_second(aSecond)
+    {}
+
+    F& first()  { return m_first; }
+    const F& first() const { return m_first; }
+    S& second() { return  m_second; }
+    const S& second() const { return  m_second; }
+};
+
+
+#endif //TEST13_1_PAIR_H
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Pair1.h"
+#include "Pair.h"
 
 void task1(){
     Pair1<int> p1(6, 9);
@@ -9,9 +10,19 @@ void task1(){
     std::cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
 }
 
-int main() {
+void task2(){
+    Pair<int, double> p1(6, 7.8);
+    std::cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
 
+    const Pair<double, int> p2(3.4, 5);
+    std::cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
+}
+
+int main() {
+    
     task1();
+
+    task2();
 
     return 0;
 }
