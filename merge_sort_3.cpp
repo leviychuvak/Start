5c2d2ffabab4c957849077f88968b5271b99679c
leviychuvak <anton1234maistrenko@gmail.com>
2022-08-21 00:51:28 +0300

    add MergeSort() function realization

--- merge_sort_3.cpp
+++ merge_sort_3.cpp
@@ -1,4 +1,4 @@
-#include "test_runner.h"
+﻿#include "test_runner.h"
 #include <algorithm>
 #include <memory>
 #include <vector>
@@ -7,18 +7,41 @@ using namespace std;
 
 template <typename RandomIt>
 void MergeSort(RandomIt range_begin, RandomIt range_end) {
-  // Напишите реализацию функции,
-  // не копируя сортируемые элементы
+	// Напишите реализацию функции,
+	// не копируя сортируемые элементы
+	if (range_end - range_begin < 2) return;
+
+	vector<typename RandomIt::value_type> elements(
+		std::make_move_iterator(range_begin), 
+		std::make_move_iterator(range_end));
+
+	auto third = std::distance(elements.begin(), elements.end()) / 3;
+
+	MergeSort(elements.begin(), elements.begin() + third);
+	MergeSort(elements.begin() + third, elements.begin() + 2 * third);
+	MergeSort(elements.begin() + 2 * third, elements.end());
+
+	vector<typename RandomIt::value_type> temp;
+	
+	std::merge(
+		elements.begin(), elements.begin() + third,
+		elements.begin() + third, elements.begin() + 2 * third,
+		std::back_inserter(temp));
+	std::merge(
+		temp.begin(), temp.end(),
+		elements.begin() + 2 * third, elements.end(),
+		range_begin
+	);
 }
 
 void TestIntVector() {
-  vector<int> numbers = {6, 1, 3, 9, 1, 9, 8, 12, 1};
-  MergeSort(begin(numbers), end(numbers));
-  ASSERT(is_sorted(begin(numbers), end(numbers)));
+	vector<int> numbers = { 6, 1, 3, 9, 1, 9, 8, 12, 1 };
+	MergeSort(begin(numbers), end(numbers));
+	ASSERT(is_sorted(begin(numbers), end(numbers)));
 }
 
 int main() {
-  TestRunner tr;
-  RUN_TEST(tr, TestIntVector);
-  return 0;
+	TestRunner tr;
+	RUN_TEST(tr, TestIntVector);
+	return 0;
 }
