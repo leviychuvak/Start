674d2f550a26785938a43fc8e479eba2cc8ace1b
leviychuvak <anton1234maistrenko@gmail.com>
2022-09-13 22:36:16 +0300

    replace the second find operation in map

--- Reference-to-element.cpp
+++ Reference-to-element.cpp
@@ -1,20 +1,22 @@
 ﻿#include <iostream>
 #include <map>
+#include <optional>
 
 template <typename T1, typename T2>
 T2& GetRefStrict(std::map<T1, T2>& m, const T1& key) {
-    //if(m.count(key)) return m[key]; //before C++20
-    
-    if (m.contains(key)) return m[key]; //Since C++20
-
-    else throw std::runtime_error("Map does not contain this key");
+    try{
+        T2& t = m.at(key);
+        return t;
+    }catch (std::out_of_range &e) {}
+    throw std::runtime_error("Map does not contain this key");
+    // или лучше пробросить out_of_range дальше в caller?
 }
 
 int main()
 {
     std::map<int, std::string> m = { {0, "value"} };
     try{
-        std::string& item = GetRefStrict(m, 0);
+        std::string& item = GetRefStrict(m, 1);
         item = "newvalue";
     }
     catch (const std::runtime_error& e){
