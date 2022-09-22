0e8f52ce7b3bb0e5abeece3a846d52739eee94ac
leviychuvak <anton1234maistrenko@gmail.com>
2022-09-22 23:05:46 +0300

    add changes

--- Person.cpp
+++ Person.cpp
@@ -1,34 +1,32 @@
 #include "Person.h"
 
-void Person::ChangeFirstName(int year, const std::string& first_name)
+void Person::ChangeFirstName(int year, std::string first_name)
 {
-	firstNames[year] = first_name;
+	firstNames.insert({ year, std::move(first_name) });
 }
 
-void Person::ChangeLastName(int year, const std::string& last_name)
+void Person::ChangeLastName(int year,  std::string last_name)
 {
-	lastNames[year] = last_name;
+	lastNames.insert({ year, std::move(last_name) });
 }
 
 std::string Person::GetFullName(int year) const
 {
-	bool fn = true;
-	bool ln = true;
-	auto fnIter = firstNames.upper_bound(year);
-	auto lnIter = lastNames.upper_bound(year);
+	bool firstnameExist = true;
+	bool lastnameExist = true;
+	auto nextFirstnameIter = firstNames.upper_bound(year);
+	auto nextLastnameIter = lastNames.upper_bound(year);
 	std::string res{};
 
-	if (firstNames.empty() || fnIter == firstNames.begin())
-		fn = false;
-	if (lastNames.empty() || lnIter == lastNames.begin())
-		ln = false;
+	firstnameExist = !firstNames.empty() && nextFirstnameIter != firstNames.begin();
+	lastnameExist = !lastNames.empty() && nextLastnameIter != lastNames.begin();
 
-	if (fn && ln)
-		return (--fnIter)->second + " " + (--lnIter)->second;
-	if (fn && !ln)
-		return (--fnIter)->second + " with unknown last name";
-	if (!fn && ln)
-		return (--lnIter)->second + " with unknown first name";
+	if (firstnameExist && lastnameExist)
+		return (--nextFirstnameIter)->second + " " + (--nextLastnameIter)->second;
+	if (firstnameExist && !lastnameExist)
+		return (--nextFirstnameIter)->second + " with unknown last name";
+	if (!firstnameExist && lastnameExist)
+		return (--nextLastnameIter)->second + " with unknown first name";
 
 	return "Incognito";
 }
--- Person.h
+++ Person.h
@@ -6,8 +6,8 @@
 
 class Person {
 public:
-	void ChangeFirstName(int year, const std::string& first_name);
-	void ChangeLastName(int year, const std::string& last_name);
+	void ChangeFirstName(int year, const std::string first_name);
+	void ChangeLastName(int year, const std::string last_name);
 	std::string GetFullName(int year) const;
 private:
 	std::map<int, std::string> firstNames;
